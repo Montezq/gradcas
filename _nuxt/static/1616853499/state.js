9231c053ee5a8942682a59bf2a061a17563240be
gavani <intalmaci21@gmail.com>
2021-03-27 16:19:08 +0200

    first

---
+++ _nuxt/static/1616853499/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgradcas\u002F_nuxt\u002Fstatic\u002F1616853499",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fgradcas\u002F",assetsPath:"\u002Fgradcas\u002F_nuxt\u002F",cdnURL:a}}}}(null));
